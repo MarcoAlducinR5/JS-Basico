73739734f9ba602d838af67782094197f2ade76a
Marco Antonio Alducin Garcia <sokram_r5_1992@hotmail.com>
2023-08-15 17:19:31 -0600

    ejemplos de variables en javascript

---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Marco";
+
+// Comentarios con //
+
+// Declarar 
+var edad;
+// Inicializar
+edad = 30;
+
+edad;
+
+var elementos = ["computadora", "celular"];
+
+var persona = {
+    nombre: "Marco",
+    edad: 30
+}
+
+persona;
