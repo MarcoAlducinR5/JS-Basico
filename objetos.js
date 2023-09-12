2eea734376ff3008d570ad7e59132428eadb5f01
Marco Antonio Alducin Garcia <sokram_r5_1992@hotmail.com>
2023-09-11 19:37:53 -0600

    Ejercicio con objetos en JavaScript

--- objetos.js
+++ objetos.js
@@ -30,3 +30,24 @@ var autoNuevo = new auto("Tesla", "Model 3", 2020);
 var autoNuevo2 = new auto("Tesla", "Model X", 2018);
 
 var autoNuevo3 = new auto("Toyota", "Carolla", 2020);
+
+function solution(car) {
+    car.licensePlate ? car.drivingLicense = true : car.drivingLicense = false;
+    return car;
+}
+
+// Prueba 1
+solution({
+    color: 'red',
+    brand: 'Kia',
+});
+
+// Prueba 2
+solution({
+    color: 'gray',
+    brand: 'Chevrolet',
+    licensePlate: 'AAA111',
+});
+
+// Prueba 3
+solution({ licensePlate: 'RGB255'  });
